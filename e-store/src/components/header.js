452c820eeec5ad2539eb90c7331e9d4f2ea95a0b
Tom Abbott <twabbott@outlook.com>
2018-06-03 23:14:17 -0600

    Some work on the header

--- e-store/src/components/header.js
+++ e-store/src/components/header.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Grid, Row, Col } from "react-bootstrap";
+
+import "./header.css";
 /*
 TODO:
 
@@ -13,42 +16,51 @@ expands to become an edit-box with the prompt "search".
 
 Add some font-awesome icons for search, account/sign-in, and the cart.  Align that stuff right.
 
-*/
+*/ 
 export default (props) => {
     return (
-        <Navbar inverse collapseOnSelect>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <a href="#">
-                        <img className="img-responsive" src="../img/spatula-city-logo.png" alt="logo" />
-                    </a>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                    <NavItem eventKey={1} href="#">
-                        Link
-                    </NavItem>
-                    <NavItem eventKey={2} href="#">
-                        Link
-                    </NavItem>
-                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                        <MenuItem eventKey={3.1}>Action</MenuItem>
-                        <MenuItem eventKey={3.2}>Another action</MenuItem>
-                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                        <MenuItem divider />
-                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                    </NavDropdown>
-                </Nav>
-                <Nav pullRight>
-                    <NavItem eventKey={1} href="#">
-                        Link Right
-                    </NavItem>
-                    <NavItem eventKey={2} href="#">
-                        Link Right
-                    </NavItem>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>);
+        <div>
+            <Row className="header">
+                <Col xs={3}>
+                <a href="#">
+                    <img className="img-responsive" src="../img/spatula-city-logo.png" alt="logo" />
+                </a>
+                </Col>
+                <Col xs={9} className="motto">
+                    "We do spatulas, and that's all!"
+                </Col>
+            </Row>
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">
+                            Link
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            Link
+                        </NavItem>
+                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">
+                            Link Right
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            Link Right
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>);
 }
